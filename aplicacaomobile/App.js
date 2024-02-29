59e1f6e97b9aabcba37e0cf09a8c69d0aa2761c5
Marcia <marciacarina931@gmail.com>
2024-02-29 09:39:59 -0400

    app com expo

--- aplicacaomobile/App.js
+++ aplicacaomobile/App.js
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Inicio from './src/inicio';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>oi linha</Text>
+      <Inicio/>
       <StatusBar style="auto" />
     </View>
   );
